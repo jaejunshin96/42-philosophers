84100ff89d0f9a5c2f51f1c677669d799e15921d
Jaejun Shin <jaeshinkorean@gmail.com>
2023-09-22 18:54:15 +0930

    save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 16:26:20 by jaeshin           #+#    #+#             */
-/*   Updated: 2023/09/20 21:47:46 by jaeshin          ###   ########.fr       */
+/*   Updated: 2023/09/22 17:25:54 by jaeshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_philo	*init_philo(void)
 {
-	t_philo	*philo;
+	t_philo		*philo;
 
 	philo = (t_philo *)malloc(sizeof(t_philo));
 	if (philo == NULL)
@@ -22,34 +22,55 @@ t_philo	*init_philo(void)
 		perror("init philo");
 		exit(1);
 	}
+	pthread_mutex_init(&philo->mutex, NULL);
 	philo->time_to_die = 100;
 	philo->time_to_eat = 20;
 	philo->time_to_sleep = 20;
 	return (philo);
 }
 
-void	*print_random(void *p)
+void	*death_timer(void *p)
 {
-	t_philo	*philo;
+	t_philo		*philo;
 
 	philo = (t_philo *)p;
-	printf("to die - %d\n", philo->time_to_die);
+	pthread_mutex_lock(&philo->mutex);
+	printf("thread 1 to die - %d\n", philo->time_to_die);
 	philo->time_to_die -= 50;
-	// printf("to die - %d\n", philo->to_die);
+	pthread_mutex_unlock(&philo->mutex);
+	return (NULL);
+}
+
+void	*death_timer2(void *p)
+{
+	t_philo		*philo;
+
+	philo = (t_philo *)p;
+	pthread_mutex_lock(&philo->mutex);
+	printf("thread 2 to die - %d\n", philo->time_to_die);
+	philo->time_to_die -= 20;
+	pthread_mutex_unlock(&philo->mutex);
 	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo		*philo;
-	pthread_t	thread;
+	t_philo			*philo;
+	pthread_t		thread;
+	pthread_t		thread2;
+	struct timeval	tv;
 
 	(void)argc;
 	(void)argv;
 	philo = init_philo();
-	// usleep(10000);
-	pthread_create(&thread, NULL, print_random, (void *)philo);
+	gettimeofday(&tv, NULL);
+	printf("tv sec - %lld ms\n",(long long) tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	pthread_create(&thread, NULL, death_timer, (void *)philo);
+	usleep(1);
+	pthread_create(&thread2, NULL, death_timer2, (void *)philo);
 	pthread_join(thread, NULL);
+	pthread_join(thread2, NULL);
+	pthread_mutex_destroy(&philo->mutex);
 	printf("time to die - %d\n", philo->time_to_die);
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:46:06 by jaeshin           #+#    #+#             */
-/*   Updated: 2023/09/20 17:47:35 by jaeshin          ###   ########.fr       */
+/*   Updated: 2023/09/22 13:26:01 by jaeshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <pthread.h>
 
 typedef struct s_philo {
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
+	pthread_mutex_t	mutex;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
 }	t_philo;
 
 #endif
